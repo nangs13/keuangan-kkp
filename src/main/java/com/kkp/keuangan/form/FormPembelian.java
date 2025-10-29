605ad97b2640c3fca6b2198c461c31b784ff34ee
Andhika putra iswara <andikajaed25@gmail.com>
2025-10-29 19:48:44 +0700

    ongoing

--- src/main/java/com/kkp/keuangan/form/FormPembelian.java
+++ src/main/java/com/kkp/keuangan/form/FormPembelian.java
@@ -1,5 +1,22 @@
 package com.kkp.keuangan.form;
 
-public class FormPembelian {
-    
+import java.util.function.Supplier;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class FormPembelian extends JFrame {
+    private JTable headerTablel;
+    private JTable detaiTable;
+    private DefaultTableModel headerTableModel;
+    private DefaultTableModel detailTableModel;
+    private JComboBox<SupplierItem> cbSupplier;
+    private JButton btnTambahItem, btnSimpan, btnHapusItem, btnRefreshSupplier;
+    private JLabel lblInfo;
+    private SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+
 }
